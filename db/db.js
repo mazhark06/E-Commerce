da46c98446671e6bcb594f8d460fa9235d3730f6
mazhark06 <153361874+mazhark06@users.noreply.github.com>
2025-07-19 18:11:47 +0530

    Delete db directory

--- db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
-
-try {
-    mongoose.connect(`${process.env.MONGODB_URI}`).catch((error)=> console.error('ERROR: CONNECTION' ,error))
-} catch (error) {
-    console.error('ERROR: MONGODB NOT CONNECTED' , error )
-}
