af6b2834d35a04b729b7366a83cd469917521b80
mazhark06 <153361874+mazhark06@users.noreply.github.com>
2025-07-19 18:11:31 +0530

    Delete Routes directory

--- Routes/sign-up.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router()
-
-
-const {savedata} = require('../controller/signup.controller.js') 
-
-router.post('/' , savedata)
-module.exports = router
